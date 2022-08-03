387d83235147a214d84b86938f003b9c8e9ebde9
Patricia <missdang@live.com>
2022-08-02 23:55:44 -0700

    Home page added, PD added. #2

--- src/components/PD.js
+++ src/components/PD.js
@@ -2,7 +2,12 @@ function PD() {
 
   return(
     <div id="PD">
-      <h3>PD component</h3>
+      <h1>Patricia Dang</h1>
+        <h3>FS SWE</h3>
+
+        <a href="https://en.wikipedia.org/wiki/Hawaii" target="_blank">
+          <img src="https://images.livemint.com/img/2021/03/14/689x388/Doublerainbow_1615733709921_1615733723659.jpg"/>
+        </a>
     </div>
   )
 }
